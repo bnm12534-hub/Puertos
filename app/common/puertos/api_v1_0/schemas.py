7b3c79702597fcc95595354cfc3053219a5b4137
Mikel <bnm12534@gmail.com>
2026-03-04 11:46:03 +0100

    Estamos en app/common/puertos/api_v1_0/schemas.py aqui vamos a utilizar para definir los esquemas flask-Marshmallow y serializar, como podemos ver hemos creado tres esquemas cada una para cada modelo Astillero, Diseñador y Velero, en la mayoria de de las clases en el id tiene 'id' con 'dump_only=true esto hara solo se fije en ese a la hora de serializar, tambien en la clase DisenadorSchema y AstilleroSchema se define a veleros con Nested que significa que puede tener varios veleros y el 'many=true' indica que contiene varios elementos de tipo VeleroSchema, em la clase Velero tiene dos variables que se define igual con Nested pero este nos va a devolver el nombre y el id del astillero y del diseñador del velero

---
+++ app/common/puertos/api_v1_0/schemas.py
@@ -0,0 +1,26 @@
+from marshmallow import fields
+from app.ext import ma
+class AstilleroSchema(ma.Schema):
+    id = fields.Int(dump_only=True)
+    nombre = fields.Str(required=True)
+    pais = fields.Str()
+    ciudad = fields.Str()
+    año_fundacion = fields.Int()
+    veleros = fields.Nested('VeleroSchema', many=True, exclude=('astillero',))
+class DisenadorSchema(ma.Schema):
+    id = fields.Int(dump_only=True)
+    nombre = fields.Str(required=True)
+    nacionalidad = fields.Str()
+    año_nacimiento = fields.Int()
+    veleros = fields.Nested('VeleroSchema', many=True, exclude=('disenador',))
+class VeleroSchema(ma.Schema):
+    id = fields.Int(dump_only=True)
+    nombre = fields.Str(required=True)
+    eslora = fields.Float()
+    manga = fields.Float()
+    calado = fields.Float()
+    año_fabricacion = fields.Int()
+    astillero_id = fields.Int(load_only=True)
+    disennador_id = fields.Int(load_only=True)
+    astillero = fields.Nested(AstilleroSchema, only=('id', 'nombre'))
+    disenador = fields.Nested(DisenadorSchema, only=('id', 'nombre'))
