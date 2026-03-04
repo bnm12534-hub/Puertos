d7692d479176e9a52045f702d8f2adee38880c10
Mikel <bnm12534@gmail.com>
2026-03-04 12:11:17 +0100

    Estamos en app/common/puertos/api_v1_0/resources.py primero vamos a definir un Blueprint para agrupar las rutas, luego creamos la API asociada al Blueprint, despues se instancia los schemas los cuales lo guardamos en una variable y nos va ayudar a validar los datos que nos lleguen y convertir los objetos de la base de datos en JSON, las clases ...ListResource(Resource) nos va a ayudar nos a dar mediante GET todos los registros y POST recibo datos Json los valida schema crea una instancia del modelo y lo guarda en la base de datos, los ...Resource(Resource) va manejar de manera individual que obtengamos mediante el id, GET va obtener un en concreto, PUT acutalizarlo y DELETE eliminarlo. En caso de que no existe se nos va a lanzar una exception que nos diga que no se ha encontrado a ninguno. Finalmente el 'api.add_resource' registran rutas y se conectan clases correspondientes por ejemplo '/api/v1.0/puertos/' para listar todos y '/api/v1.0/puertos/<int:id> uno en especifico

---
+++ app/common/puertos/api_v1_0/resources.py
@@ -0,0 +1,87 @@
+from flask import request, Blueprint
+from flask_restful import Resource, Api
+
+from app.common.error_handling import ObjectNotFound
+
+from .schemas import AstilleroSchema, DisenadorSchema, VeleroSchema
+from ..models import Astillero, Disenador, Velero
+
+puertos_v1_0_bp = Blueprint('puertos_v1_0', __name__)
+api = Api(puertos_v1_0_bp)
+
+astillero_schema = AstilleroSchema()
+disenador_schema = DisenadorSchema()
+velero_schema = VeleroSchema()
+
+class AstilleroListResource(Resource):
+    def get(self):
+ 
+        astilleros = Astillero.get_all()
+        return astillero_schema.dump(astilleros, many=True)
+
+    def post(self):
+ 
+        data = request.get_json()
+        astillero_dict = astillero_schema.load(data)
+        astillero = Astillero(**astillero_dict)
+        astillero.save()
+        return astillero_schema.dump(astillero), 201
+
+
+class AstilleroResource(Resource):
+    def get(self, id):
+        astillero = Astillero.get_by_id(id)
+        if astillero is None:
+            raise ObjectNotFound("Astillero no encontrado")
+        return astillero_schema.dump(astillero)
+
+    def put(self, id):
+        astillero = Astillero.get_by_id(id)
+        if astillero is None:
+            return {"message": "Astillero no encontrado"}, 404
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(astillero, key, value)
+        astillero.save()
+        return astillero_schema.dump(astillero)
+
+    def delete(self, id):
+        astillero = Astillero.get_by_id(id)
+        if astillero is None:
+            raise ObjectNotFound("Astillero no encontrado")
+        astillero.delete()
+        return {"message": "Astillero eliminado"}, 204
+
+
+class DisenadorListResource(Resource):
+    def get(self):
+        disenadores = Disenador.get_all()
+        return disenador_schema.dump(disenadores, many=True)
+
+    def post(self):
+        data = request.get_json()
+        disenador_dict = disenador_schema.load(data)
+        disenador = Disenador(**disenador_dict)
+        disenador.save()
+        return disenador_schema.dump(disenador), 201
+
+
+
+class VeleroListResource(Resource):
+    def get(self):
+        veleros = Velero.get_all()
+        return velero_schema.dump(veleros, many=True)
+
+    def post(self):
+        data = request.get_json()
+        velero_dict = velero_schema.load(data)
+        velero = Velero(**velero_dict)
+        velero.save()
+        return velero_schema.dump(velero), 201
+
+
+
+api.add_resource(AstilleroListResource, '/api/v1.0/puertos/', endpoint='astilleros_list')
+api.add_resource(AstilleroResource, '/api/v1.0/puertos/<int:id>', endpoint='astillero_detail')
+api.add_resource(DisenadorListResource, '/api/v1.0/disenadores/', endpoint='disenadores_list')
+api.add_resource(VeleroListResource, '/api/v1.0/veleros/', endpoint='veleros_list')
