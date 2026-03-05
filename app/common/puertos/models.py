abfb067bb07abeec8d1e2a73751c84884497ef72
Mikel <bnm12534@gmail.com>
2026-03-05 10:41:40 +0100

    He modificado forms.py y aqui se validada si lo puesto en el formulario esta correcto, y tambien en models.py añadi la clase  user

--- app/common/puertos/models.py
+++ app/common/puertos/models.py
@@ -1,5 +1,19 @@
 from app.db import db, BaseModelMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
+class User(db.Model, BaseModelMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password=generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 class Astillero(db.Model, BaseModelMixin):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,8 @@
-from flask import FlaskForm
-from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, IntegerField
+from wtforms.validators import DataRequired, Email
 
-class SignForm(FlaskForm):
-    name = StringField('Nombre', validators=[DataRequired(), Length(max=80)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()]) 
-    submit = SubmitField('Registrar')
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired()])
+    submit = StringField('Login')
